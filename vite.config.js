f6c78f07af7e6e89ae6f2bc16183829abbfdec45
subinsong01 <haely97@naver.com>
2025-03-18 16:28:14 +0900

    chore: 세팅

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'book-job',
+        short_name: 'book-job',
+        description: '구직활동과 커뮤니티를 한 곳에서',
+        theme_color: '#ffffff',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+  
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
