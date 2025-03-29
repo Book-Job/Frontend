89700d5b3b0a4f03b023ef7d16d87c20123458e2
subinsong01 <haely97@naver.com>
2025-03-29 17:40:44 +0900

    style: 버튼 컴포넌트, 메인탭 버튼, 마이페이지 버튼 웹 버전 완성

--- src/components/web/BigButton.jsx
+++
@@ -1,9 +0,0 @@
-const BigButton = ({ label, bgColor = '#E36397' }) => {
-  return (
-    <button className='w-[332px] h-[58px] rounded-[5px]' style={{ backgroundColor: bgColor }}>
-      <span className='font-bold text-[20px] text-white'>{label}</span>
-    </button>
-  )
-}
-
-export default BigButton
