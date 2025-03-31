2520c9dccf32c9f7fdd2c9712e1b4537bebfb87f
subinsong01 <haely97@naver.com>
2025-03-31 17:45:27 +0900

    style: ScrollToTopButton 완성

---
+++ src/components/web/ScrollToTopButton.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+const ScrollTopButton = ({ icon, onClick }) => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <button
+        className='w-[50px] h-[50px] rounded-full bg-[#FFFFFF] border-[0.5px] border-[#8E8E8E] flex items-center justify-center'
+        onClick={onClick}
+      >
+        <img src={icon} alt='위로 올리는 버튼' className='w-[10px] h-[13px]' />
+      </button>
+    </div>
+  )
+}
+
+ScrollTopButton.propTypes = {
+  icon: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+export default ScrollTopButton
