18fff5fa2af959c49fe70ca969dd0d42e01ffdab
subinsong01 <haely97@naver.com>
2025-03-31 23:23:03 +0900

    style: 모바일 버튼 컴포넌트 완성

---
+++ src/components/app/MobileButton.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+const MobileButton = ({ label, size = 'big', bgColor = '#d9d9d9', onClick }) => {
+  const buttonSize = {
+    small: 'w-[92px] h-[45px] text-[16px]',
+    big: 'w-[336px] h-[50px] text-[18px]',
+  }
+
+  return (
+    <button
+      className={`${buttonSize[size]} rounded-[5px]`}
+      style={{ backgroundColor: bgColor }}
+      onClick={() => onClick(label)}
+    >
+      <span className='font-bold text-white'>{label}</span>
+    </button>
+  )
+}
+
+MobileButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'big']),
+  bgColor: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default MobileButton
