b70d4e80052110a940fed6e1448cfbf0c22c9656
EunSeok-222 <dldmstjr114@naver.com>
2025-04-02 00:57:54 +0900

    style:MobileWorkBoard 컴포넌트 추가

---
+++ src/stories/MobileWorkBoard.stories.jsx
@@ -0,0 +1,42 @@
+import { action } from '@storybook/addon-actions';
+import MobileWorkBoard from '../components/app/MobileWorkBoard';
+
+
+export default {
+  title: 'components/web/MobileWorkBoard',
+  component: MobileWorkBoard,
+  args: {
+    title: '외주 줍니다...',
+    onClick: action('클릭액션'),
+    name: '하하호호', 
+    view: 122 ,
+    date: '2020-11-11', 
+    like: true,
+    popular1: true,
+    joboffer1: true,
+    history1: true,
+    jobsearch1: true,
+    othersite1: true,
+    worktype1: true,
+    
+  },
+}
+
+// export const Default = (args) => <WorkBoard {...args} />
+const Template = (args) => <MobileWorkBoard {...args} />
+
+export const Test = Template.bind({})
+Test.args = {
+  title: '외주 줍니다...',
+  onClick: action('클릭액션'),
+  name: '하하호호', 
+  view: 122 , 
+  date: '2020-11-11', 
+  like: true,
+  popular1: true,
+  joboffer1: true,
+  history1: true,
+  jobsearch1: true,
+  othersite1: true,
+  worktype1: true,
+}
