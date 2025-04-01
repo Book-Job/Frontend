e52ac56920b6f6102c2fba2b5ea965a5db0a6b5e
EunSeok-222 <dldmstjr114@naver.com>
2025-04-02 00:00:53 +0900

    style:ShareViews에 링크 컴포넌트 추가

--- src/components/web/ShareViews.jsx
+++ src/components/web/ShareViews.jsx
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types'
 
-const ShareViews = ({label,icon,textColor}) => {
+const ShareViews = ({ label, icon, textColor, weblink }) => {
   return (
     <div className='inline-flex items-center gap-1 w-auto h-[24px] bg-none px-1'>
       {icon && <img src={icon} alt={label} className='w-[16px] h-[16px]' />}
-      <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
+      {weblink ? (
+        <a href={weblink} target='_blank' rel='noopener noreferrer'>
+          <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
+        </a>
+      ) : (
+        <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
+      )}
     </div>
-  );
-};
+  )
+}
 
 ShareViews.propTypes = {
   label: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
+  weblink: PropTypes.string.isRequired,
   textColor: PropTypes.string.isRequired,
 }
 
-export default ShareViews;
+export default ShareViews
