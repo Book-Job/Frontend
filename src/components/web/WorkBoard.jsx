9d863690e937cb9c66e45ae21c46c7a33337b839
EunSeok-222 <dldmstjr114@naver.com>
2025-04-01 15:00:38 +0900

    WorkBoard 완성

--- src/components/web/WorkBoard.jsx
+++ src/components/web/WorkBoard.jsx
@@ -6,20 +6,31 @@ import jobsearch from '../../assets/icons/common/common_tag_jobsearch.svg'
 import othersite from '../../assets/icons/common/common_tag_othersite.svg'
 import popular from '../../assets/icons/common/common_tag_popular.svg'
 import worktype from '../../assets/icons/common/common_tag_worktype.svg'
+import viewPink from '../../assets/icons/common/common_view_pink.svg'
+import ShareViews from './ShareViews'
 const WorkBoard = ({ title, name, date, like }) => {
   return (
     <div className='w-[300px] h-[200px]'>
-      <div className=' h-full  border border-[#D6D6D6] rounded-[10px] p-[25px]'>
-        <div className='flex gap-2'>
-          <TagIcon label='인기 글' icon={popular} />
-          <TagIcon label='구인' icon={joboffer} />
-          <TagIcon label='경력' icon={history} />
-          <TagIcon label='구직' icon={jobsearch} />
-          <TagIcon label='외부 사이트' icon={othersite} />
-          <TagIcon label='정규직' icon={worktype} />
+      <div className='flex flex-col h-full  border border-[#D6D6D6] rounded-[10px] p-[25px] justify-between'>
+        <div className='flex-row'>
+          <div className='flex gap-2 mb-2'>
+            {/* <TagIcon label='인기 글' icon={popular} /> */}
+            <TagIcon label='구인' icon={joboffer} />
+            <TagIcon label='경력' icon={history} />
+            {/* <TagIcon label='구직' icon={jobsearch} /> */}
+            {/* <TagIcon label='외부 사이트' icon={othersite} /> */}
+            <TagIcon label='정규직' icon={worktype} />
+          </div>
+          <div className='text-[18px] font-bold'>{title}</div>
         </div>
-
-        <div className=''>{title}</div>
+        <div className='flex-row  text-[#8E8E8E] text-[14px]'>
+          <div className='flex justify-end font-bold '>이름{name}</div>
+          <hr className='border-[#8E8E8E] my-2' />
+          <div className='flex justify-between'>
+            2025-03-05{date}
+            <ShareViews label='123' textColor='text-[#E36397]' icon={viewPink} />
+          </div>
+        </div>
       </div>
     </div>
   )
