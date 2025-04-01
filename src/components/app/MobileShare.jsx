57ceab6007f5bfffef9b6003f09ee4c7ba5fae31
EunSeok-222 <dldmstjr114@naver.com>
2025-04-02 00:28:59 +0900

    style:MobileShare컴포넌트 추가

--- src/components/app/MobileShare.jsx
+++ src/components/app/MobileShare.jsx
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types'
 
-const ShareViews = ({ label, icon, textColor, weblink }) => {
+const MobileShare = ({ label, icon, textColor, weblink }) => {
   return (
     <div className='inline-flex items-center gap-1 w-auto h-[24px] bg-none px-1'>
-      {icon && <img src={icon} alt={label} className='w-[18px] h-[18px]' />}
+      {icon && <img src={icon} alt={label} className='w-[16px] h-[16px]' />}
       {weblink ? (
         <a href={weblink} target='_blank' rel='noopener noreferrer'>
-          <span className={`flex font-semibold text-[14px] ${textColor}`}>{label}</span>
+          <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
         </a>
       ) : (
-        <span className={`flex font-semibold text-[14px] ${textColor}`}>{label}</span>
+        <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
       )}
     </div>
   )
 }
 
-ShareViews.propTypes = {
+MobileShare.propTypes = {
   label: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
   weblink: PropTypes.string.isRequired,
   textColor: PropTypes.string.isRequired,
 }
 
-export default ShareViews
+export default MobileShare
