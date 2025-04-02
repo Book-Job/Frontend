86720ec41c032c41c90c50d54637861032883cae
subinsong01 <haely97@naver.com>
2025-04-02 16:40:35 +0900

    fix: 충돌 해결

--- src/components/web/FreeBoard.jsx
+++ src/components/web/FreeBoard.jsx
@@ -3,23 +3,23 @@ import viewPink from '../../assets/icons/common/common_view_pink.svg'
 import comment from '../../assets/icons/common/comment.svg'
 
 import ShareViews from './ShareViews'
-const FreeBoard = ({ title, content, name, date, onClick }) => {
+const FreeBoard = ({ title, content, name, date, onClick, comment1, view1 }) => {
   return (
     <div className='w-[300px] h-[200px]'>
-      <div className='flex flex-col h-full  border border-[#D6D6D6] rounded-[10px] px-[25px] pt-[25px] pb-[16px] justify-between'>
+      <div className='flex flex-col h-full  border border-[#D6D6D6] rounded-[10px] px-[25px] pt-[25px] pb-[20px] justify-between'>
         <div className='flex-row' onClick={onClick}>
           <div className='text-[18px] font-bold line-clamp-1'>{title}</div>
           <div className='mt-[10px] text-[16px] line-clamp-2'>{content}</div>
         </div>
         <div className='flex-row  text-gray-8e8e8e text-[14px]'>
           <div onClick={onClick} className='flex justify-between font-bold '>
-            <ShareViews label='123' textColor='text-gray-8e8e8e' icon={comment} />
+            <ShareViews label={comment1} textColor='text-gray-8e8e8e' icon={comment} />
             <div>{name}</div>
           </div>
-          <hr className='border-gray-8e8e8e my-2' />
+          <hr className='my-1 border-gray-8e8e8e' />
           <div className='flex items-end justify-between'>
             {date}
-            <ShareViews label='123' textColor='text-[#E36397]' icon={viewPink} />
+            <ShareViews label={view1} textColor='text-[#E36397]' icon={viewPink} />
           </div>
         </div>
       </div>
@@ -32,6 +32,8 @@ FreeBoard.propTypes = {
   content: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
+  comment1: PropTypes.number.isRequired,
+  view1: PropTypes.number.isRequired,
   onClick: PropTypes.func,
 }
 export default FreeBoard
--- src/components/web/WorkBoard.jsx
+++ src/components/web/WorkBoard.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import TagIcon from './TagIcon'
 import joboffer from '../../assets/icons/common/common_tag_ joboffer.svg'
 import history from '../../assets/icons/common/common_tag_history.svg'
 import jobsearch from '../../assets/icons/common/common_tag_jobsearch.svg'
@@ -9,8 +8,22 @@ import worktype from '../../assets/icons/common/common_tag_worktype.svg'
 import viewPink from '../../assets/icons/common/common_view_pink.svg'
 import bookmarkGray from '../../assets/icons/common/common_bookmark_gray.svg'
 import bookmarkPink from '../../assets/icons/common/common_bookmark_pink.svg'
+import TagIcon from './TagIcon'
 import ShareViews from './ShareViews'
-const WorkBoard = ({ title, name, date, like, onClick }) => {
+const WorkBoard = ({
+  title,
+  name,
+  date,
+  like,
+  onClick,
+  popular1,
+  joboffer1,
+  history1,
+  jobsearch1,
+  othersite1,
+  worktype1,
+  view,
+}) => {
   const bookmarkIcon = like === true ? bookmarkPink : bookmarkGray
   return (
     <div className='w-[300px] h-[200px]'>
@@ -20,17 +33,17 @@ const WorkBoard = ({ title, name, date, like, onClick }) => {
         onClick={onClick}
         className='w-[23px] h-[23px] relative absolute top-[20px] left-[255px] '
       />
-      <div className='flex flex-col h-full  border border-[#D6D6D6] rounded-[10px] px-[25px] pt-[25px] pb-[16px] justify-between'>
+      <div className='flex flex-col h-full  border border-[#D6D6D6] rounded-[10px] px-[25px] pt-[25px] pb-[20px] justify-between'>
         <div className='flex-row'>
           <div className='flex flex-wrap gap-2 mb-2'>
-            {/* <TagIcon label='인기 글' icon={popular} /> */}
-            {/* <TagIcon label='구인' icon={joboffer} /> */}
-            <TagIcon label='경력 1~3년' icon={history} />
-            <TagIcon label='구직' icon={jobsearch} />
-            {/* <TagIcon label='외부 사이트' icon={othersite} /> */}
-            <TagIcon label='정규직' icon={worktype} />
+            {popular1 === true ? <TagIcon label='인기 글' icon={popular} /> : ''}
+            {joboffer1 === true ? <TagIcon label='구인' icon={joboffer} /> : ''}
+            {history1 === true ? <TagIcon label='경력 1~3년' icon={history} /> : ''}
+            {jobsearch1 === true ? <TagIcon label='구직' icon={jobsearch} /> : ''}
+            {othersite1 === true ? <TagIcon label='외부 사이트' icon={othersite} /> : ''}
+            {worktype1 === true ? <TagIcon label='정규직' icon={worktype} /> : ''}
           </div>
-          <div onClick={onClick} className='text-[18px] font-bold'>
+          <div onClick={onClick} className='text-[18px] font-bold line-clamp-2'>
             {title}
           </div>
         </div>
@@ -38,10 +51,10 @@ const WorkBoard = ({ title, name, date, like, onClick }) => {
           <div onClick={onClick} className='flex justify-end font-bold '>
             {name}
           </div>
-          <hr className='border-gray-8e8e8e my-2' />
+          <hr className='my-1 border-gray-8e8e8e' />
           <div className='flex items-end justify-between'>
             {date}
-            <ShareViews label='123' textColor='text-[#E36397]' icon={viewPink} />
+            <ShareViews label={view} textColor='text-[#E36397]' icon={viewPink} />
           </div>
         </div>
       </div>
@@ -52,8 +65,15 @@ const WorkBoard = ({ title, name, date, like, onClick }) => {
 WorkBoard.propTypes = {
   title: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  view: PropTypes.number.isRequired,
   date: PropTypes.string.isRequired,
   like: PropTypes.bool.isRequired,
+  popular1: PropTypes.bool.isRequired,
+  joboffer1: PropTypes.bool.isRequired,
+  history1: PropTypes.bool.isRequired,
+  jobsearch1: PropTypes.bool.isRequired,
+  othersite1: PropTypes.bool.isRequired,
+  worktype1: PropTypes.bool.isRequired,
   onClick: PropTypes.func,
 }
 export default WorkBoard
