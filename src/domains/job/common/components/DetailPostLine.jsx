29622bf1cdb1e245e279115a7ac58eda09ae03ac
subinsong01 <haely97@naver.com>
2025-04-04 19:46:29 +0900

    style: 공통 컴포넌트 분리

---
+++ src/domains/job/common/components/DetailPostLine.jsx
@@ -0,0 +1,4 @@
+const DetailPostLine = () => {
+  return <span className='block w-[940px] h-[3px] mt-[60px] bg-gray-8e8e8e' />
+}
+export default DetailPostLine
---
+++ src/domains/job/common/components/LastFormLine.jsx
@@ -0,0 +1,4 @@
+const LastFormLine = () => {
+  return <div className='w-[940px] h-[1px] bg-gray-8e8e8e' />
+}
+export default LastFormLine
