b57e080635e6b38ac31f788fd811b48db8bd7f65
subinsong01 <haely97@naver.com>
2025-04-04 16:12:29 +0900

    style: footer mt 추가

--- src/components/web/Footer.jsx
+++ src/components/web/Footer.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 
-const Footer = ({ email,onClick }) => {
+const Footer = ({ email, onClick }) => {
   return (
-    <div className='flex w-full h-[157px] bg-[#FDF8FA] justify-center gap-24 items-center px-10'>
+    <div className='flex w-full h-[157px] bg-[#FDF8FA] justify-center gap-24 items-center px-10 mt-[40px]'>
       <div className='text-[50px] font-bold text-[#E36397]'>bookjob</div>
       <div className='flex text-[16px] flex-row gap-10 text-[#8E8E8E]'>
         <div className=''>팀장 | 이신지</div>
         <div className=''>팀원 | 김영철, 이은석, 송수빈</div>
-        <div className='' onClick={onClick}>이메일 | {email}</div>
+        <div className='' onClick={onClick}>
+          이메일 | {email}
+        </div>
       </div>
     </div>
   )
