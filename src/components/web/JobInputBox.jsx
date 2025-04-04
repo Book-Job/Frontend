8ee231bec0f9f046d397fd46a28f73fd9c40f6ad
subinsong01 <haely97@naver.com>
2025-04-04 20:07:50 +0900

    feat: useRef 추가

--- src/components/web/JobInputBox.jsx
+++ src/components/web/JobInputBox.jsx
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types'
+import React from 'react'
 
-const JobInputBox = ({ placeholder }) => {
+const JobInputBox = React.forwardRef(({ placeholder, className = '', ...props }, ref) => {
   return (
     <div className='flex flex-col items-start justify-center'>
       <input
+        ref={ref}
         type='text'
         placeholder={placeholder}
-        className='w-[686px] h-[58px] border border-gray-8e8e8e rounded px-4 text-black focus:border-main-color-pink focus:outline-none'
+        className={`w-[686px] h-[58px] border border-gray-8e8e8e rounded px-4 text-black focus:border-main-color-pink focus:outline-none ${className}`}
+        {...props}
       />
     </div>
   )
-}
+})
+
+JobInputBox.displayName = 'JobInputBox'
 
 JobInputBox.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
 
 export default JobInputBox
