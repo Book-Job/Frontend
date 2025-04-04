e06b417b18b5f8b0e1c799971242a11a0dc97d62
subinsong01 <haely97@naver.com>
2025-04-04 19:49:24 +0900

    style: 구인&구직 퍼블리싱 api 연동 전 상태 완성

--- src/domains/job/main/JobMainPage.jsx
+++ src/domains/job/main/JobMainPage.jsx
@@ -1,4 +1,4 @@
-import Header from '../../../components/web/Header'
+import BannerExample from '../../../components/common/BannerExample'
 import SearchBar from '../../../components/web/SearchBar'
 import WorkBoard from '../../../components/web/WorkBoard'
 import CountPost from './components/CountPost'
@@ -8,8 +8,7 @@ import JobPostSortDropDown from './components/JobPostSortDropDown'
 const JobMainPage = () => {
   return (
     <>
-      <Header />
-      <div className='w-full h-[314px] bg-light-gray'></div>
+      <BannerExample />
       <SearchBar className='mt-[45px] ml-[264px]' />
       <div className='flex items-center mt-[53px]'>
         <CountPost />
