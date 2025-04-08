ba9e55d60ff59d57f71491ff84e88e94a6a8ec2d
EunSeok-222 <dldmstjr114@naver.com>
2025-04-08 16:45:02 +0900

    feat:메인 버튼 기능 추가

--- src/domains/main/common/components/BoardButton.jsx
+++ src/domains/main/common/components/BoardButton.jsx
@@ -2,31 +2,42 @@ import { useNavigate } from 'react-router-dom'
 import ROUTER_PATHS from '../../../../routes/RouterPath'
 import commonTime from '../../../../assets/icons/common/common_time.svg'
 
-const BoardButton = () => {
+const BoardButton = ({ onBoardSelect, selectedBoard }) => {
   const boardButton = [
     {
       name: '자유',
-      path: ROUTER_PATHS.LOGIN_MAIN,
+      // path: ROUTER_PATHS.LOGIN_MAIN,
     },
     {
       name: '구인구직',
-      path: '/',
+      // path: '/',
     },
   ]
   const navigate = useNavigate()
   // const today = new Date().toISOString().split('T')[0]
   const today = new Date()
   const formattedDate = `${today.getMonth() + 1}월 ${today.getDate()}일`
+
+  const handleClick = (path, name) => {
+    navigate(path)
+    onBoardSelect(name === '자유' ? '자유게시판' : '구인구직') // 버튼 이름에 따라 보드 이름 설정
+  }
   return (
     <div className='w-[940px] '>
       <div className='relative flex items-end justify-center'>
         <p className='flex gap-3'>
           {boardButton.map(({ name, path }, index) => {
+            const isSelected =
+              (name === '자유' && selectedBoard === '자유게시판') ||
+              (name === '구인구직' && selectedBoard === '구인구직')
             return (
               <button
                 key={index}
-                onClick={() => navigate(path)}
-                className='bg-light-gray w-[157px] h-[47px] border rounded-full text-2xl'
+                // onClick={() => navigate(path)}
+                onClick={() => handleClick(path, name)}
+                className={`w-[157px] h-[47px] border rounded-full text-2xl ${
+                  isSelected ? 'bg-main-color-pink text-white' : 'bg-light-gray text-black'
+                }`}
               >
                 {name}
               </button>
--- src/domains/main/main/MainPage.jsx
+++ src/domains/main/main/MainPage.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import BestList from '../common/components/BestList'
 import BoardButton from '../common/components/BoardButton'
 
 const MainPage = () => {
+  const [selectedBoard, setSelectedBoard] = useState('자유게시판')
+  const handleBoardSelect = (boardName) => {
+    setSelectedBoard(boardName)
+  }
   return (
     <div className='flex flex-col items-center w-full'>
       <div className='mb-10'>
-        <BoardButton />
+        <BoardButton onBoardSelect={handleBoardSelect} selectedBoard={selectedBoard}/>
       </div>
       <div className=''>
-        <BestList />
+        {selectedBoard === '자유게시판' && <BestList boardName='자유게시판' />}
+        {selectedBoard === '구인구직' && <BestList boardName='구인구직' />}
       </div>
     </div>
   )
