d0fa8c7a1c7afaeae5fcbfb80a8fbbaa251399ed
subinsong01 <haely97@naver.com>
2025-04-08 16:25:57 +0900

    style: spinner 추가

---
+++ src/components/web/Spinner.jsx
@@ -0,0 +1,18 @@
+const Spinner = ({ size = 48, color = 'main-pink' }) => {
+  const borderColorClass =
+    {
+      'main-pink': 'border-main-pink',
+    }[color] || 'border-main-pink'
+
+  return (
+    <div
+      className={`animate-spin rounded-full border-4 border-t-transparent ${borderColorClass}`}
+      style={{
+        width: size,
+        height: size,
+      }}
+    ></div>
+  )
+}
+
+export default Spinner
