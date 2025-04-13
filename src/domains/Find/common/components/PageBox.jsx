20d9f184859026e91872e94e349643a2f93de10f
EunSeok <dldmstjr114@naver.com>
2025-04-13 19:45:07 +0900

    style: FindPwCheckIDPage 완성,경로 수정

--- src/domains/Find/common/components/PageBox.jsx
+++ src/domains/Find/common/components/PageBox.jsx
@@ -1,6 +1,6 @@
 const PageBox = ({ children }) => {
   return (
-    <div className='border rounded-[5px] border-dark-gray p-10 mt-7 w-full max-w-[657px]'>
+    <div className='border rounded-[5px] border-dark-gray px-10 py-14 mt-7 w-full max-w-[657px]'>
       {children}
     </div>
   )
