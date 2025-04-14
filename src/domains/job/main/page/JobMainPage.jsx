d62474943e4c54da65b492373bebd29a1fddc328
subinsong01 <haely97@naver.com>
2025-04-14 15:50:11 +0900

    style: 구인&구직 메인 페이지 반응형까지 완성

--- src/domains/job/main/page/JobMainPage.jsx
+++ src/domains/job/main/page/JobMainPage.jsx
@@ -1,5 +1,6 @@
 import SearchBar from '../../../../components/web/SearchBar'
 import WorkBoard from '../../../../components/web/WorkBoard'
+import MobileWorkBoard from '../../../../components/app/MobileWorkBoard'
 import CountPost from '../components/CountPost'
 import JobDropDown from '../components/JobDropDown'
 import JobPostSortDropDown from '../components/JobPostSortDropDown'
@@ -7,25 +8,22 @@ import JobPostSortDropDown from '../components/JobPostSortDropDown'
 const JobMainPage = () => {
   return (
     <>
-      <SearchBar className='mt-[45px] ml-[264px]' placeholder='검색어를 입력하세요' />
-      <div className='flex items-center mt-[53px]'>
-        <CountPost />
-        <JobDropDown />
-        <JobPostSortDropDown />
+      <div className='flex justify-center'>
+        <SearchBar placeholder='검색어를 입력하세요' />
       </div>
-      <div className='ml-[254px] flex gap-4'>
-        <WorkBoard />
-        <div className='mx-4'>
-          <WorkBoard />
+      <div className='px-4 md:px-[100px] mt-2 lg:px-[250px]'>
+        <div className='flex items-center justify-between'>
+          <CountPost />
+          <div className='flex items-end'>
+            <JobDropDown />
+            <JobPostSortDropDown className='text-xs sm:text-sm md:text-[15px] font-semibold' />
+          </div>
         </div>
-        <WorkBoard />
-      </div>
-      <div className='ml-[254px] flex gap-4 mt-6'>
-        <WorkBoard />
-        <div className='mx-4'>
-          <WorkBoard />
+
+        <div className='flex gap-4 mt-4 mx-[27px]'>
+          <WorkBoard className='hidden sm:block' />
+          <MobileWorkBoard className='block sm:hidden' />
         </div>
-        <WorkBoard />
       </div>
     </>
   )
