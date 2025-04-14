f44f15bffae8d3aa1347b7dee3250b8cefcdb423
subinsong01 <haely97@naver.com>
2025-04-14 18:15:50 +0900

    style: 구인 글 작성 페이지 반응형 & react-hook-form 적용

--- src/domains/job/common/components/FormItem.jsx
+++ src/domains/job/common/components/FormItem.jsx
@@ -1,12 +1,20 @@
 import JobLabel from '../../../../components/web/JobLabel'
 
-const FormItem = ({ label, dot, children }) => (
-  <div className='flex flex-col sm:flex-row sm:items-center gap-2 mb-4'>
-    <div className='min-w-[70px] sm:min-w-[210px]'>
-      <JobLabel label={label} dot={dot} className='text-[16px] sm:text-[24px]  sm:mr-[30px]' />
+const FormItem = ({ label, dot, children }) => {
+  const isSpecialRow = label === '구인 / 구직'
+
+  return (
+    <div
+      className={`flex ${
+        isSpecialRow ? 'flex-row items-center' : 'flex-col sm:flex-row sm:items-center'
+      } gap-2 mb-4`}
+    >
+      <div className='min-w-[70px] sm:min-w-[210px]'>
+        <JobLabel label={label} dot={dot} className='text-[16px] sm:text-[24px] sm:mr-[30px]' />
+      </div>
+      <div className='flex-1'>{children}</div>
     </div>
-    <div className='flex-1'>{children}</div>
-  </div>
-)
+  )
+}
 
 export default FormItem
