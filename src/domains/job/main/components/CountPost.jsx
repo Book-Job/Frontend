9088d7597d7cff461bd152db455af98106805da9
subinsong01 <haely97@naver.com>
2025-04-14 15:50:47 +0900

    style: 반응형에 맞게 수정

--- src/domains/job/main/components/CountPost.jsx
+++ src/domains/job/main/components/CountPost.jsx
@@ -1,10 +1,10 @@
 const CountPost = () => {
   return (
-    <>
-      <span className='font-semibold text-[15px] ml-[254px]'>오늘 5건 </span>
-      <span className='font-semibold text-[15px] ml-2 mr-2'>|</span>
-      <span className='font-semibold text-[15px] mr-[625px]'> 총 5,240건 </span>
-    </>
+    <div className='flex justify-between items-center px-7 mt-4 text-xs sm:text-sm md:text-[15px] font-semibold'>
+      <span>오늘 5건</span>
+      <span className='mx-2'>|</span>
+      <span>총 5,240건</span>
+    </div>
   )
 }
 
