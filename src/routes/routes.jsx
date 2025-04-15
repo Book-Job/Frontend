43d53e790887df31316cef2dd21bf481aab3d838
EunSeok-222 <dldmstjr114@naver.com>
2025-04-15 15:12:58 +0900

    stlye:EditProfile페이지 완성

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,20 +1,22 @@
-import MemberDataEntry from '../domains/login/main/MemberDataEntry'
-import MainPage from '../domains/main/main/MainPage'
 import ROUTER_PATHS from './RouterPath'
-import JobMainPage from '../domains/job/main/JobMainPage'
+import JobMainPage from '../domains/job/main/page/JobMainPage'
 import CommunityMainPage from '../domains/community/main/page/CommunityMainPage'
-import WriteCommunityPost from '../domains/community/write/page/WriteCommunityPost'
-import WriteRecruitmentPosting from '../domains/job/recruitment/WriteRecruitmentPosting'
-import WriteJobSearchPosting from '../domains/job/job-search/WriteJobSearchPosting'
-import DetailJobSearchPost from '../domains/job/job-search/DetailJobSearchPost'
-import FindIDPage from '../domains/Find/main/FindIDPage'
-import FindPwPage from '../domains/Find/main/FindPwPage'
+import WriteCommunityPostPage from '../domains/community/write/page/WriteCommunityPostPage'
+import WriteRecruitmentPostPage from '../domains/job/recruitment/page/WriteRecruitmentPostPage'
+import WriteJobSearchPostPage from '../domains/job/search/page/WriteJobSearchPostPage'
 import FindPwCheckIDPage from '../domains/Find/detail/FindPwCheckIDPage'
 import ChangePwPage from '../domains/Find/detail/ChangePwPage'
 import FindIDCompletePage from '../domains/Find/detail/FindIDCompletePage'
 import UserPosts from '../domains/community/detail/page/UserPosts'
-import MyPage from '../domains/my/main/MyPage'
-import EditProfile from '../domains/my/detail/EditProfile'
+import Join from './../domains/login/page/Join';
+import MainPage from './../domains/main/main/MainPage';
+import LoginMain from './../domains/login/page/LoginMain';
+import MemberDataEntry from './../domains/login/page/MemberDataEntry';
+import FindIDPage from './../domains/Find/page/FindIDPage';
+import FindPwPage from './../domains/Find/page/FindPwPage';
+import MyPage from './../domains/my/page/MyPage';
+import EditProfile from './../domains/my/detail/EditProfile';
+import EditPassword from '../domains/my/detail/EditPassword'
 
 const routes = [
   {
@@ -34,10 +36,6 @@ const routes = [
     element: <MemberDataEntry />,
   },
   {
-    path: ROUTER_PATHS.JOB_MAIN,
-    element: <JobMainPage />,
-  },
-  {
     path: ROUTER_PATHS.COMMUNITY,
     element: <CommunityMainPage />,
   },
@@ -47,19 +45,39 @@ const routes = [
   },
   {
     path: ROUTER_PATHS.WRITE_COMMUNITY_POST,
-    element: <WriteCommunityPost />,
+    element: <WriteCommunityPostPage />,
+  },
+  {
+    path: ROUTER_PATHS.JOB_MAIN,
+    element: <JobMainPage />,
   },
   {
     path: ROUTER_PATHS.WRITE_RECRUITMENT_POST,
-    element: <WriteRecruitmentPosting />,
+    element: <WriteRecruitmentPostPage />,
   },
   {
     path: ROUTER_PATHS.WRITE_JOB_SEARCH_POST,
-    element: <WriteJobSearchPosting />,
+    element: <WriteJobSearchPostPage />,
   },
   {
-    path: ROUTER_PATHS.JOB_SEARCH_POST_DETAIL,
-    element: <DetailJobSearchPost />,
+    path: ROUTER_PATHS.FIND_ID,
+    element: <FindIDPage />,
+  },
+  {
+    path: ROUTER_PATHS.FIND_ID_COMPLETE_PAGE,
+    element: <FindIDCompletePage />,
+  },
+  {
+    path: ROUTER_PATHS.FIND_PW,
+    element: <FindPwPage />,
+  },
+  {
+    path: ROUTER_PATHS.FIND_PW_CHECK_ID_PAGE,
+    element: <FindPwCheckIDPage />,
+  },
+  {
+    path: ROUTER_PATHS.FIND_PW_CHANGE_PW,
+    element: <ChangePwPage />,
   },
   {
     path: ROUTER_PATHS.FIND_ID,
@@ -84,12 +102,15 @@ const routes = [
   {
     path: ROUTER_PATHS.MY_PAGE,
     element: <MyPage />,
-    noMargin: true,
+    noMargin:true ,
   },
   {
     path: ROUTER_PATHS.MY_EDIT_PROFILE,
     element: <EditProfile />,
-    noMargin: true,
+  },
+  {
+    path: ROUTER_PATHS.MY_EDIT_PW,
+    element: <EditPassword />,
   },
 ]
 
