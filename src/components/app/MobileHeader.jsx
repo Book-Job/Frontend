72823936ed79a45cc4c88bf3e16f337a346890d7
subinsong01 <haely97@naver.com>
2025-04-15 18:06:24 +0900

    style: 모바일 헤더 Route 일관서 있게 삽입

--- src/components/app/MobileHeader.jsx
+++ src/components/app/MobileHeader.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types'
+import headerArrow from '../../assets/icons/mobile/mobile_header_arrow.svg'
 
-const MobileHeader = ({ icon, label, onClick }) => {
+const MobileHeader = ({ icon = headerArrow, label, onClick }) => {
   return (
-    <button className='flex items-center gap-[6px]' onClick={onClick}>
-      <img src={icon} alt='뒤로가기 버튼' className='w-[7px] h-[12px] mr-[115px]' />
-      <span className='text-[22px] font-bold'>{label}</span>
-    </button>
+    <div className='relative w-full mt-[25px]'>
+      <button className='absolute left-[24px] top-1/2 -translate-y-1/2' onClick={onClick}>
+        <img src={icon} alt='뒤로가기 버튼' className='w-[12px] h-[24px]' />
+      </button>
+      <span className='block text-[22px] font-bold text-center'>{label}</span>
+    </div>
   )
 }
 
 MobileHeader.propTypes = {
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string,
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 }
