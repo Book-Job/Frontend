8de5bb762bea01359c7780fbd8fc972efc9407f9
subinsong01 <haely97@naver.com>
2025-04-15 18:07:30 +0900

    style: navigate 삽입 후 스토리북에서도 사용 가능하게 수정

--- src/stories/MobileMainHeader.stories.jsx
+++ src/stories/MobileMainHeader.stories.jsx
@@ -1,9 +1,17 @@
 import { action } from '@storybook/addon-actions'
 import MobileMainHeader from '../components/app/MobileMainHeader'
+import { MemoryRouter } from 'react-router-dom'
 
 export default {
   title: 'components/app/MobileMainHeader',
   component: MobileMainHeader,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
   args: {
     login: '',
     onClick: action('클릭액션'),
