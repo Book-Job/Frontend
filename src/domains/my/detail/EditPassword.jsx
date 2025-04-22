49b0f8d29fc2afad9cb9f51d712b755c6de283e8
EunSeok <dldmstjr114@naver.com>
2025-04-22 21:40:35 +0900

    wip:중간저장

--- src/domains/my/detail/EditPassword.jsx
+++ src/domains/my/detail/EditPassword.jsx
@@ -3,20 +3,20 @@ import PageTitle from '../../Find/common/components/PageTitle'
 import PageBox from './../../Find/common/components/PageBox'
 import ROUTER_PATHS from '../../../routes/RouterPath'
 import { useNavigate } from 'react-router-dom'
+import LabelWithInput from '../../../components/web/LabelWithInput'
+import Button from '../../../components/web/Button'
 
 const EditPassword = () => {
   const {
     register,
-    watch,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm()
 
   const onSubmit = (data) => {
-    const { emailId, passwordCheck, ...filteredData } = data // passwordCheck 필터링
-    console.log('폼 데이터:', filteredData)
-    navigate(ROUTER_PATHS.FIND_ID_COMPLETE_PAGE)
+    // const { emailId, passwordCheck, ...filteredData } = data // passwordCheck 필터링
+    console.log('사용중인 비밀번호:', data)
+    navigate(ROUTER_PATHS.FIND_PW_CHANGE_PW)
   }
 //작업중 페이지
   const navigate = useNavigate()
@@ -25,7 +25,34 @@ const EditPassword = () => {
       <PageTitle title={'비밀번호 변경'} />
       <div className='flex justify-center'>
         <PageBox>
-          
+        <div className='flex justify-start text-3xl font-bold'>비밀번호 입력</div>
+            <div className='flex-auto mt-8'>
+              <LabelWithInput
+                label='비밀번호'
+                type='text'
+                placeholder='현재 비밀번호를 입력해주세요'
+                size='biggest'
+                {...register('userPW', { required: '현재 사용중인 비밀번호와 일치하지않습니다' })}
+              />
+            </div>
+            <div className='flex items-start'>
+              {errors.userPW && <p className='text-red-500 text-[14px]'>{errors.userPW.message}</p>}
+            </div>
+            <div className='flex items-end mt-6'>
+              <Button
+                size='biggest'
+                label='다음'
+                bgColor='light-gray'
+                onClick={handleSubmit(onSubmit)}
+              />
+            </div>
+            {/* <div>
+              <FindLink
+                title={'가입한 아이디를 잊어버렸어요!'}
+                onClick={() => navigate(ROUTER_PATHS.FIND_ID)}
+                linkName={'아이디 찾기'}
+              />
+            </div> */}
         </PageBox>
       </div>
     </div>
