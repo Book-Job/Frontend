b2dc013fb35fd73888da42a16e7cbf346a8cc6d0
EunSeok-222 <dldmstjr114@naver.com>
2025-04-29 23:50:53 +0900

    wip: aip -> api수정

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseAip = axios.create({
+export const baseApi = axios.create({
   baseURL: 'http://43.200.107.80:8080/api/v1',
   withCredentials: true,
 });
