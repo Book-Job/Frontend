4fadce3490ca0b7238230a575e3cb9ca4d826699
subinsong01 <haely97@naver.com>
2025-04-29 14:30:49 +0900

    feat: axiosInstance 적용

--- src/domains/community/service/postService.js
+++ src/domains/community/service/postService.js
@@ -1,31 +1,27 @@
-import axios from 'axios'
+import { publicApi } from '../../../services/api/Axios'
 
-//전체 글 조회
+// 전체 글 조회
 export const getAllPosts = async () => {
-  const response = await axios.get('/api/v1/boards')
+  const response = await publicApi.get('/boards')
   return response.data.data
 }
 
-//글 작성
+// 글 작성(누구나 작성가능)
 export const createPost = async (postData) => {
-  const response = await axios.post('/api/v1/boards', postData)
+  const response = await publicApi.post('/boards', postData)
   return response.data
 }
 
-//글 상세 조회
+// 글 상세 조회
 export const getDetailPosts = async (id) => {
-  const response = await axios.get(`/api/v1/boards/${id}`)
+  const response = await publicApi.get(`/boards/${id}`)
   return response.data.data
 }
 
-// 글 검색(search-bar)
+// 글 검색
 export const searchPosts = async ({ title, email }) => {
-  const response = await axios.get('/api/v1/boards', {
+  const response = await publicApi.get('/boards', {
     params: { title, email },
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
   })
   return response.data
 }
