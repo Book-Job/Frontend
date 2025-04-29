60a84a51e61643a91639a2d725c6d40728957795
subinsong01 <haely97@naver.com>
2025-04-29 17:06:19 +0900

    feat: 페이지 이동시에 맨 위로 올려주는 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import PageScrollToTop from './components/common/PageScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <PageScrollToTop />
       <AppRoutes />
     </BrowserRouter>
   )
