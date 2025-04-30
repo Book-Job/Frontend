9e691294a596a8714ae31e2f2890ca14a1221279
subinsong01 <haely97@naver.com>
2025-04-30 16:44:30 +0900

    feat: authApi로 수정

--- src/domains/job/recruitment/service/postService.js
+++ src/domains/job/recruitment/service/postService.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import { authApi } from '../../../../services/api/Axios'
 
 //구인 글 작성
 export const createPost = async (postData) => {
-  const response = await axios.post('/api/v1/job-posting', postData)
+  const response = await authApi.post('/job-posting', postData)
   return response.data
 }
