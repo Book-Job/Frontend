a06de5779d798af782ca064066790f5c3ddb9a5d
subinsong01 <haely97@naver.com>
2025-05-01 12:23:45 +0900

    feat: 구직 input form 컴포넌트화

---
+++ src/domains/job/search/components/form/WorkExperience.jsx
@@ -0,0 +1,21 @@
+import FormItem from '../../../common/components/FormItem'
+import JobInputBox from '../../../../../components/web/JobInputBox'
+
+const WorkExperience = ({ register, errors }) => {
+  return (
+    <FormItem label='경력' dot={false}>
+      <JobInputBox
+        placeholder='ex) 신입 / 경력 3~5 년'
+        {...register('experience', { required: '경력을 입력하세요' })}
+      />
+
+      <div className='flex items-start'>
+        {errors.experience && (
+          <span className='text-red-500 text-[14px] mt-1'>{errors.experience.message}</span>
+        )}
+      </div>
+    </FormItem>
+  )
+}
+
+export default WorkExperience
