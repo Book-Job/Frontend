741b9af4c377cf862fb5d1ddb7d0abe02776b4f7
subinsong01 <haely97@naver.com>
2025-05-01 12:24:42 +0900

    feat: 구직&구인 글 작성 api > authApi로 수정

--- src/domains/job/recruitment/service/postService.js
+++
@@ -1,7 +0,0 @@
-import { authApi } from '../../../../services/api/Axios'
-
-//구인 글 작성
-export const createPost = async (postData) => {
-  const response = await authApi.post('/job-posting', postData)
-  return response.data
-}
--- src/domains/job/search/service/postService.js
+++
@@ -1,7 +0,0 @@
-import { authApi } from '../../../../services/api/Axios'
-
-//구직 글 작성
-export const createPost = async (postData) => {
-  const response = await authApi.post('/api/v1/job-seeking', postData)
-  return response.data
-}
