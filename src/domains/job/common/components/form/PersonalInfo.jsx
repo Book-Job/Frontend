55002a6cf0b5849b3268c2c75357c5fcc1b1d3b0
subinsong01 <haely97@naver.com>
2025-05-02 19:52:58 +0900

    feat: id 받아오기(현재 nickname 못 불러오는 상태)

--- src/domains/job/common/components/form/PersonalInfo.jsx
+++ src/domains/job/common/components/form/PersonalInfo.jsx
@@ -1,33 +1,16 @@
 import FormItem from '../FormItem'
-import JobInputBox from '../../../../../components/web/JobInputBox'
 import JobFormLine from '../JobFormLine'
+import useAuthStore from '../../../../../store/login/useAuthStore'
 
-//받아와야하는 데이터
 const PersonalInfo = ({ register }) => {
+  const { user } = useAuthStore()
+
+  //현재 nickname이 콘솔에 안 찍힘,, 아이디만 찍히고 있어서 일단 아이디로 처리
   return (
     <>
       <div className='my-[10px]'>
-        <FormItem label='닉네임' dot={false} register={register} name='writer'>
-          <JobInputBox
-            placeholder='ex) 홍길동'
-            className='w-full sm:w-[171px]'
-            {...register('writer', { required: '닉네임은 필수입니다' })}
-          />
-        </FormItem>
-      </div>
-      <JobFormLine />
-      <div className='my-[30px]'>
-        <FormItem label='이메일' dot={false} register={register} name='email'>
-          <JobInputBox
-            placeholder='example@email.com'
-            {...register('email', {
-              required: '이메일은 필수입니다',
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: '유효한 이메일을 입력해주세요',
-              },
-            })}
-          />
+        <FormItem label='닉네임' dot={false} name='writer'>
+          <div className='flex sm:w-[171px] md:text-[14px]'>{user?.sub ?? '닉네임 없음'}</div>
         </FormItem>
       </div>
       <JobFormLine />
