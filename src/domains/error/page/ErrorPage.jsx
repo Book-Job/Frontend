9f831f7f913e925b0b637baa566140d8590389b1
subinsong01 <haely97@naver.com>
2025-05-10 16:11:06 +0900

    style: 임시 에러페이지 생성

---
+++ src/domains/error/page/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>404 - 페이지를 찾을 수 없습니다 😢</h1>
+      <p>주소가 잘못되었거나, 존재하지 않는 페이지입니다.</p>
+    </div>
+  )
+}
+
+export default ErrorPage
