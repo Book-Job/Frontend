4cb158ef8e268122c6862e4f719ce7052992c024
subinsong01 <haely97@naver.com>
2025-05-10 15:25:52 +0900

    style: 폼 스타일 수정

--- src/domains/job/common/components/LastFormLine.jsx
+++ src/domains/job/common/components/LastFormLine.jsx
@@ -1,5 +1,3 @@
 const LastFormLine = () => {
-  return <div className='w-full sm:w-[870px] h-[1px] bg-dark-gray' />
+  return <div className='w-full sm:w-[955px] h-[1px] bg-dark-gray mb-[30px]' />
 }
-
-export default LastFormLine
