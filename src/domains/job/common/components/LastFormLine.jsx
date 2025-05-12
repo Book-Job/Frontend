5b575a3d5c8ab0cdfae49bb3c6ed417dec0d73c8
subinsong01 <haely97@naver.com>
2025-05-12 23:55:48 +0900

    style: 스타일 수정

--- src/domains/job/common/components/LastFormLine.jsx
+++ src/domains/job/common/components/LastFormLine.jsx
@@ -1,5 +1,5 @@
 const LastFormLine = () => {
-  return <div className='w-full sm:w-[955px] h-[1px] bg-dark-gray mb-[30px]' />
+  return <div className='w-full sm:w-[870px] h-[1px] bg-dark-gray mb-[20px]' />
 }
 
 export default LastFormLine
