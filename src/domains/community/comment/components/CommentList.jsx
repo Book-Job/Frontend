6c11287b4a297f2fae7b6ee83710ffa14e5b3f74
subinsong01 <haely97@naver.com>
2025-05-13 15:54:10 +0900

    feat: 자유게시판 댓글 불러오기 & 퍼블리싱 완성

--- src/domains/community/comment/components/CommentList.jsx
+++ src/domains/community/comment/components/CommentList.jsx
@@ -1,13 +1,24 @@
 const CommentList = ({ comments }) => {
   if (!comments || comments.length === 0) {
-    return <p>작성된 댓글이 없습니다.</p>
+    return <p className='text-gray-500 mt-4'>작성된 댓글이 없습니다.</p>
   }
+
   return (
-    <div>
-      {comments.map((comment) => (
+    <div className='w-full mt-4 border border-dark-gray rounded-md overflow-hidden'>
+      {comments.map((comment, index) => (
         <div key={comment.commentId}>
-          <div>{comment.nickname}</div>
-          <div>{comment.text}</div>
+          <div className='px-4 py-3'>
+            <div className='flex justify-between items-center mb-1'>
+              <span className='font-semibold text-gray-800'>{comment.nickname}</span>
+            </div>
+            <p className='text-gray-700 whitespace-pre-wrap flex justify-between items-center mt-2'>
+              {comment.text}
+            </p>
+          </div>
+          <span className='text-[13px] text-dark-gray flex justify-between items-center mb-1 ml-4'>
+            {new Date(comment.createdAt).toLocaleDateString()}
+          </span>
+          {index !== comments.length - 1 && <hr className='border-t border-light-gray mx-4' />}
         </div>
       ))}
     </div>
