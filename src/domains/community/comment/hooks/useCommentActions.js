d9d2f310c71eda1fe78ef6adae434b1c7eeac57a
subinsong01 <haely97@naver.com>
2025-05-13 23:53:58 +0900

    feat: 댓글 불러오기 zustand 방식으로 수정

--- src/domains/community/comment/hooks/useCommentActions.js
+++ src/domains/community/comment/hooks/useCommentActions.js
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { deleteCommentService, editCommentService } from '../../service/commentService'
 
 const useCommentActions = () => {
-  const [loading, setLoading] = useState(false)
-
   const deleteComment = async (boardId, commentId) => {
-    setLoading(true)
     try {
-      await axios.delete(`/api/v1/boards/${boardId}/comment/${commentId}`)
+      await deleteCommentService(boardId, commentId)
     } catch (error) {
       console.error('댓글 삭제 실패:', error)
-    } finally {
-      setLoading(false)
+      throw error
     }
   }
 
   const editComment = async (boardId, commentId, newContent) => {
-    setLoading(true)
     try {
-      await axios.patch(`/api/v1/boards/${boardId}/comment/${commentId}`, {
-        content: newContent,
-      })
+      await editCommentService(boardId, commentId, newContent)
     } catch (error) {
-      console.error('댓글 수정 실패:', error)
-    } finally {
-      setLoading(false)
+      throw error
     }
   }
 
-  return { deleteComment, editComment, loading }
+  return { deleteComment, editComment }
 }
 
 export default useCommentActions
