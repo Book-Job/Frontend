4d0768b74a2842960974ff7263c59eef0360ee2a
subinsong01 <haely97@naver.com>
2025-05-15 01:08:47 +0900

    feat: 날짜만 보이게 수정

--- src/domains/job/recruitment/components/form/ClosingDate.jsx
+++ src/domains/job/recruitment/components/form/ClosingDate.jsx
@@ -1,12 +1,18 @@
 import FormItem from '../../../common/components/FormItem'
 
-const ClosingDate = ({ register }) => {
+const ClosingDate = ({ register, setValue }) => {
+  const handleDateChange = (e) => {
+    const selectedDate = new Date(e.target.value)
+    selectedDate.setHours(18, 0, 0)
+    setValue('closingDate', selectedDate.toISOString())
+  }
+
   return (
     <FormItem label='구인마감' dot={true} register={register} name='closingDate'>
       <input
         type='date'
         className='w-full h-[58px] border border-dark-gray rounded px-4 text-black focus:border-main-pink focus:outline-none cursor-pointer'
-        {...register('closingDate')}
+        onChange={handleDateChange}
       />
       <p className='flex text-red-500 text-[13px] mt-1'>미선택시 상시채용으로 자동 입력됩니다.</p>
     </FormItem>
