ecd52b1e33350de6e6d42b1c178a8840ad13124e
EunSeok-222 <dldmstjr114@naver.com>
2025-05-14 15:29:00 +0900

    feat:베스트 글 바로가기 기능 추가

--- src/domains/main/common/components/BestList.jsx
+++ src/domains/main/common/components/BestList.jsx
@@ -24,14 +24,14 @@ const BestList = ({ boardName, bestList }) => {
         <div className='text-xl font-bold sm:text-3xl'>{boardName} 베스트</div>
       </div>
       <div className='grid grid-flow-row sm:grid-cols-2 gap-y-4 gap-x-20'>
-        {bestList.map(({ title, path, commentCount }, index) => {
+        {bestList.map(({ title, boardId, commentCount }, index) => {
           return (
             <div key={index} className='flex items-center'>
               <p className='w-5 sm:text-[20px] text-[15px] font-medium'>{index + 1}.</p>
               <div className='flex sm:text-[20px] text-[15px] font-medium items-center justify-between w-full ml-3'>
                 <button
                   className='flex justify-start w-auto line-clamp-1'
-                  onClick={() => navigate(path)}
+                  onClick={() => navigate(`/community/post/${boardId}`)}
                 >
                   {title}
                 </button>
