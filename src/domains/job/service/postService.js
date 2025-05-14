704e6a29ec85c8b432f0a9f60699d437c976a2fb
subinsong01 <haely97@naver.com>
2025-05-15 01:01:26 +0900

    feat: 구인 글 수정 & 삭제 API 요청

--- src/domains/job/service/postService.js
+++ src/domains/job/service/postService.js
@@ -1,9 +1,31 @@
-import { authApi } from '../../../services/api/Axios'
+import { authApi } from '../../../services/api/axios'
 
 //구인 글 작성
 export const createRecruitmentPost = async (postData) => {
   const response = await authApi.post('/job-posting', postData)
   return response.data
+}
+
+//구인 글 상세조회
+export const getRecruitmentPostDetail = async (jobPostingId) => {
+  const response = await authApi.get(`/job-posting/${jobPostingId}`)
+  return response.data
+}
+
+//구인 글 수정
+export const editRecruitmentPost = async (jobPostingId, data) => {
+  const response = await authApi.patch(`/job-posting/${jobPostingId}`, data, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+  return response.data
+}
+//구인 글 삭제
+export const deleteRecruitmentPost = async (jobPostingId) => {
+  const response = await authApi.delete(`/job-posting/${jobPostingId}`)
+  return response.data.data
 }
 
 //구직 글 작성
