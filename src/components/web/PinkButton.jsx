be7367c2256333c6bb924745f0e68f9ee314b939
subinsong01 <haely97@naver.com>
2025-05-15 13:58:30 +0900

    fix: 버튼 타입 제거

--- src/components/web/PinkButton.jsx
+++ src/components/web/PinkButton.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-const PinkButton = ({ label, type, ...props }) => {
+const PinkButton = ({ label, ...props }) => {
   return (
     <button className='w-[226px] h-[58px] rounded-[5px] bg-main-pink' {...props}>
       <span className='font-bold text-[20px] text-white'>{label}</span>
