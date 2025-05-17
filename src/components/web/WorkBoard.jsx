7d6b9370ebd4c74e1af60a06184714d4fd8b30d9
subinsong01 <haely97@naver.com>
2025-05-17 18:42:56 +0900

    feat: 태그 설정

--- src/components/web/WorkBoard.jsx
+++ src/components/web/WorkBoard.jsx
@@ -7,47 +7,69 @@ import popular from '../../assets/icons/common/common_tag_popular.svg'
 import worktype from '../../assets/icons/common/common_tag_worktype.svg'
 import viewPink from '../../assets/icons/common/common_view_pink.svg'
 import bookmarkGray from '../../assets/icons/common/common_bookmark_gray.svg'
+import bookmarkPink from '../../assets/icons/common/common_bookmark_pink.svg'
 import TagIcon from './TagIcon'
 import ShareViews from './ShareViews'
+import useScrapStore from '../../domains/job/scrap/store/useScrapStore'
+import { employmentTypes } from '../../domains/job/common/utils/employmentTypes'
+import getExperienceLabel from '../../domains/job/common/utils/getExperienceLabel'
+
+const getEmploymentLabel = (value) => {
+  const found = employmentTypes.find((item) => item.value === value)
+  return found ? found.label : value
+}
 
 const WorkBoard = ({
   title,
   name,
   date,
-  like: initialLike,
+  like,
   onClick,
   popular1,
   joboffer1,
-  history1,
   jobsearch1,
   othersite1,
-  worktype1,
+  employmentType,
+  experienceLabel,
   view,
   className,
+  type,
+  postId,
+  userId,
 }) => {
-  const bookmarkIcon = bookmarkGray
+  const scraps = useScrapStore((state) => state.scraps)
+  const loading = useScrapStore((state) => state.loading)
+  const toggleScrap = useScrapStore((state) => state.toggleScrap)
+
+  const scrapped = Boolean(scraps[postId])
+  const bookmarkIcon = scrapped ? bookmarkPink : bookmarkGray
+
+  const handleToggleScrap = () => {
+    toggleScrap(postId, type)
+  }
 
   return (
-    <div className={`w-[290px] h-[200px] mb-[20px] ${className}`}>
+    <div className={`w-[290px] h-[200px] mb-[20px]`}>
       <img
         src={bookmarkIcon}
         alt='bookmark'
         className='w-[23px] h-[23px] relative absolute top-[20px] left-[255px] cursor-pointer'
+        onClick={handleToggleScrap}
+        style={{ opacity: loading ? 0.5 : 1 }}
       />
       <div className='flex flex-col h-full border border-[#D6D6D6] rounded-[10px] px-[18px] pt-[15px] pb-[10px] justify-between cursor-pointer'>
-        <div className='flex-row'>
-          <div className='flex flex-wrap gap-2 mb-2'>
-            {popular1 === true ? <TagIcon label='인기 글' icon={popular} /> : ''}
-            {joboffer1 === true ? <TagIcon label='구인' icon={joboffer} /> : ''}
-            {history1 === true ? <TagIcon label='경력 1~3년' icon={history} /> : ''}
-            {jobsearch1 === true ? <TagIcon label='구직' icon={jobsearch} /> : ''}
-            {othersite1 === true ? <TagIcon label='외부 사이트' icon={othersite} /> : ''}
-            {worktype1 === true ? <TagIcon label='정규직' icon={worktype} /> : ''}
-          </div>
-          <div onClick={onClick} className='flex text-[18px] font-bold line-clamp-2'>
-            {title}
-          </div>
+        <div className='flex flex-wrap gap-2 mb-2'>
+          {popular1 && <TagIcon label='인기 글' icon={popular} />}
+          {joboffer1 && <TagIcon label='구인' icon={joboffer} />}
+          {experienceLabel && <TagIcon label={experienceLabel} icon={history} />}
+
+          {jobsearch1 && <TagIcon label='구직' icon={jobsearch} />}
+          {othersite1 && <TagIcon label='외부 사이트' icon={othersite} />}
+          {employmentType && <TagIcon label={getEmploymentLabel(employmentType)} icon={worktype} />}
         </div>
+        <h3 onClick={onClick} className='flex text-[18px] font-bold line-clamp-2'>
+          {title}
+        </h3>
         <div className='flex-row text-dark-gray text-[14px]'>
           <div onClick={onClick} className='flex justify-end font-bold'>
             {name}
@@ -71,11 +93,12 @@ WorkBoard.propTypes = {
   date: PropTypes.string.isRequired,
   like: PropTypes.bool.isRequired,
   popular1: PropTypes.bool.isRequired,
+  experienceLabel: PropTypes.string,
   joboffer1: PropTypes.bool.isRequired,
-  history1: PropTypes.bool.isRequired,
+  experienceLabel: PropTypes.string,
   jobsearch1: PropTypes.bool.isRequired,
   othersite1: PropTypes.bool.isRequired,
-  worktype1: PropTypes.bool.isRequired,
+  employmentType: PropTypes.string,
   onClick: PropTypes.func,
   className: PropTypes.string,
   userId: PropTypes.number.isRequired,
