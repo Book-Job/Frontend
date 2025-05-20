f6156f9234a55ed9db783ac3ac8a0e556d8898e8
subinsong01 <haely97@naver.com>
2025-05-20 16:18:35 +0900

    style: 반응형으로 수정

--- src/components/web/FreeBoard.jsx
+++ src/components/web/FreeBoard.jsx
@@ -25,28 +25,42 @@ const FreeBoard = ({ boardId, title, content, name, date, onClick, commentCount,
   }
 
   return (
-    <div className='w-[300px]' onClick={goToDetailPage}>
-      <div className='flex flex-col h-full border border-[#D6D6D6] rounded-[10px] px-[25px] pt-[25px] pb-[20px] justify-between cursor-pointer'>
+    <div
+      className='w-full max-w-md sm:max-w-[350px] md:max-w-[400px] mx-auto'
+      onClick={goToDetailPage}
+    >
+      <div
+        className='
+        flex flex-col h-full
+        border border-[#D6D6D6]
+        rounded-[10px]
+        px-4 sm:px-5 pt-5 pb-4
+        justify-between cursor-pointer
+        bg-white
+        shadow-sm
+        transition
+        hover:shadow-md
+      '
+      >
         <div className='flex flex-col items-start text-left' onClick={onClick}>
-          <div className='text-[18px] font-bold line-clamp-1'>{title}</div>
-          <div className='mt-[10px] text-[16px] line-clamp-2'>{content}</div>
+          <div className='text-base sm:text-lg font-bold truncate'>{title}</div>
+          <div className='mt-2 text-sm sm:text-base line-clamp-2'>{content}</div>
         </div>
 
-        <div className='text-dark-gray text-[14px] mt-[15px]'>
+        <div className='text-dark-gray text-xs sm:text-sm mt-4'>
           <div className='flex justify-between font-bold'>
             <ShareViews label={commentCount} textColor='text-dark-gray' icon={comment} />
             <div className='relative inline-block'>
               <span
                 onClick={handleNameClick}
-                className='cursor-pointer hover:underline z-10 text-sm font-medium'
+                className='cursor-pointer hover:underline z-10 text-xs sm:text-sm font-medium'
               >
                 {name}
               </span>
-
               {showButton && (
                 <button
-                  onClick={(e) => goToAuthorPosts(e)}
-                  className='absolute left-1/2 -translate-x-1/2 top-[100%] mt-[1px] bg-white text-xs text-black px-2 py-[2px] rounded-md border border-gray-300 whitespace-nowrap shadow-md'
+                  onClick={goToAuthorPosts}
+                  className='absolute left-1/2 -translate-x-1/2 top-[100%] mt-1 bg-white text-xs text-black px-2 py-1 rounded-md border border-gray-300 whitespace-nowrap shadow-md'
                 >
                   작성글 보기
                 </button>
