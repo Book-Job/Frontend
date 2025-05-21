3afec00f80036222d7bb0a6d69e77703fcda92ae
EunSeok <dldmstjr114@naver.com>
2025-05-21 14:50:06 +0900

    style:모바링 헤더 select 커스텀

--- src/components/app/MobileHeader.jsx
+++ src/components/app/MobileHeader.jsx
@@ -3,11 +3,11 @@ import headerArrow from '../../assets/icons/mobile/mobile_header_arrow.svg'
 
 const MobileHeader = ({ icon = headerArrow, label, onClick }) => {
   return (
-    <header className='relative w-full h-[50px]'>
+    <header className='relative flex w-full h-[50px]'>
       <button className='absolute left-[24px] top-1/2 -translate-y-1/2' onClick={onClick}>
         <img src={icon} alt='뒤로가기 버튼' className='w-[12px] h-[24px]' />
       </button>
-      <span className='block text-[22px] font-bold text-center'>{label}</span>
+      <span className='text-[22px] font-bold text-center flex items-center justify-center w-full'>{label}</span>
     </header>
   )
 }
