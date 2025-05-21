d925f903a81d5ed95b8febdc53fcf90661b3eda5
EunSeok <dldmstjr114@naver.com>
2025-05-21 13:30:37 +0900

    style:모바일 헤더 높이 변경

--- src/components/app/MobileHeader.jsx
+++ src/components/app/MobileHeader.jsx
@@ -3,7 +3,7 @@ import headerArrow from '../../assets/icons/mobile/mobile_header_arrow.svg'
 
 const MobileHeader = ({ icon = headerArrow, label, onClick }) => {
   return (
-    <header className='relative w-full mt-[25px]'>
+    <header className='relative w-full h-[50px]'>
       <button className='absolute left-[24px] top-1/2 -translate-y-1/2' onClick={onClick}>
         <img src={icon} alt='뒤로가기 버튼' className='w-[12px] h-[24px]' />
       </button>
