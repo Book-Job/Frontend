ee1f7bbc24aacb37633a3cd5f0a565af76a90d33
subinsong01 <haely97@naver.com>
2025-05-26 19:26:36 +0900

    style: 임시 배너 삽입

--- src/components/common/BannerExample.jsx
+++ src/components/common/BannerExample.jsx
@@ -1,5 +1,10 @@
+import BannerImage from '../../assets/banner/web/web_Banner.png'
 const BannerExample = () => {
-  return <div className='w-full h-[314px] bg-light-gray'></div>
+  return (
+    <div className='block cursor-pointer' aria-label='설문조사 폼으로 이동'>
+      <img src={BannerImage} alt='설문조사 참여 안내 배너' />
+    </div>
+  )
 }
 
 export default BannerExample
