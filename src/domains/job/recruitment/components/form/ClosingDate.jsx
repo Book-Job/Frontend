88878e36695cb504918e7d3adc5b845a4bb0dccf
subinsong01 <haely97@naver.com>
2025-05-26 16:24:12 +0900

    ftx: 코드래빗 피드백 반영 완료

--- src/domains/job/recruitment/components/form/ClosingDate.jsx
+++ src/domains/job/recruitment/components/form/ClosingDate.jsx
@@ -7,7 +7,7 @@ const ClosingDate = ({ control }) => {
     const yyyy = today.getFullYear()
     const mm = String(today.getMonth() + 1).padStart(2, '0')
     const dd = String(today.getDate()).padStart(2, '0')
-    return (minDate = `${yyyy}-${mm}-${dd}`)
+    return `${yyyy}-${mm}-${dd}`
   }, [])
 
   return (
