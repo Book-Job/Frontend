ccb735790db127adc28bdb1c817426c32802ce45
subinsong01 <haely97@naver.com>
2025-05-27 18:26:16 +0900

    feat: 구인 & 구직 정렬 구현 완료

---
+++ src/domains/job/common/constants/sortOptions.js
@@ -0,0 +1,15 @@
+export const recruitmentSortOptions = [
+  { value: 'latest', label: '최신순' },
+  { value: 'popular', label: '인기순' },
+  { value: 'closing_soon', label: '마감임박순' },
+]
+
+export const seekingSortOptions = [
+  { value: 'latest', label: '최신순' },
+  { value: 'editor', label: '에디터 최신순' },
+  { value: 'designer', label: '디자이너 최신순' },
+  { value: 'marketer', label: '마케터 최신순' },
+  { value: 'illustrator', label: '일러스트레이터 최신순' },
+  { value: 'support', label: '서포트 최신순' },
+  { value: 'etc', label: '기타 최신순' },
+]
---
+++ src/domains/job/common/utils/sortOrder.js
@@ -0,0 +1,15 @@
+export const recruitmentSortOrder = {
+  latest: 'LATEST',
+  popular: 'POPULAR',
+  closing_soon: 'CLOSING_SOON',
+}
+
+export const seekingSortOrder = {
+  latest: 'LATEST',
+  editor: 'EDITOR_LATEST',
+  designer: 'DESIGNER_LATEST',
+  marketer: 'MARKETER_LATEST',
+  illustrator: 'ILLUSTRATOR_LATEST',
+  support: 'SUPPORT_LATEST',
+  etc: 'ETC_LATEST',
+}
