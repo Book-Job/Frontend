d39da584a560afcef8c52b71f312c4784e7f79c1
subinsong01 <haely97@naver.com>
2025-06-04 15:32:22 +0900

    feat: header 고정 적용

--- src/components/app/MobileHeader.jsx
+++ src/components/app/MobileHeader.jsx
@@ -7,7 +7,9 @@ const MobileHeader = ({ icon = headerArrow, label, onClick }) => {
       <button className='absolute left-[24px] top-1/2 -translate-y-1/2' onClick={onClick}>
         <img src={icon} alt='뒤로가기 버튼' className='w-[12px] h-[24px]' />
       </button>
-      <span className='text-[22px] font-bold text-center flex items-center justify-center w-full'>{label}</span>
+      <span className='text-[22px] font-bold text-center flex items-center justify-center w-full'>
+        {label}
+      </span>
     </header>
   )
 }
