8617d8955b4d3bcc5cb89c32b83850130146eab5
subinsong01 <haely97@naver.com>
2025-06-14 21:07:07 +0900

    fix: 코드래빗 피드백 반영

--- src/domains/community/detail/page/DetailCommunityPage.jsx
+++ src/domains/community/detail/page/DetailCommunityPage.jsx
@@ -126,12 +126,7 @@ const DetailCommunityPage = () => {
 
       <div className='flex flex-wrap gap-2 mb-4 ml-0 sm:ml-5 justify-end'>
         <MobileShare label={post.viewCount.toString()} icon={viewPink} textColor='text-main-pink' />
-        <MobileShare
-          label={comments.length.toString()}
-          icon={comment}
-          textColor='text-dark-gray'
-          weblink={currentUrl}
-        />
+        <MobileShare label={comments.length.toString()} icon={comment} textColor='text-dark-gray' />
         <MobileShare
           label='공유'
           icon={share}
