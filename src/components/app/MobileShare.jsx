c73551581b53f444cc996f049b9c7abba240bafa
subinsong01 <haely97@naver.com>
2025-06-14 19:56:41 +0900

    feat: share API 적용

--- src/components/app/MobileShare.jsx
+++ src/components/app/MobileShare.jsx
@@ -1,25 +1,53 @@
 import PropTypes from 'prop-types'
+import ToastService from '../../utils/toastService'
 
-const MobileShare = ({ label, icon, textColor, weblink }) => {
+const MobileShare = ({ label, icon, textColor, weblink, post }) => {
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: `제목 : ${post.title}`,
+          text: `${post.nickname}님이 공유한 글을 확인하세요.`,
+          url: weblink,
+        })
+
+        ToastService.success('공유 성공')
+      } catch (error) {
+        if (error.name === 'AbortError') {
+          ToastService.info('공유가 취소되었습니다.')
+        } else {
+          console.error('공유 실패', error)
+          ToastService.error('공유 중 오류가 발생했습니다.')
+        }
+      }
+    } else {
+      ToastService.info('공유 기능을 지원하지 않는 브라우저입니다.')
+    }
+  }
+
   return (
-    <div className='inline-flex items-center gap-1 w-auto h-[24px] bg-none'>
+    <button
+      onClick={handleShare}
+      className={`inline-flex items-center gap-1 w-auto h-[24px] bg-none ${textColor}`}
+      type='button'
+    >
       {icon && <img src={icon} alt={label} className='w-[16px] h-[16px]' />}
-      {weblink ? (
-        <a href={weblink} target='_blank' rel='noopener noreferrer'>
-          <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
-        </a>
-      ) : (
-        <span className={`flex font-semibold text-[12px] ${textColor}`}>{label}</span>
-      )}
-    </div>
+      <span className='flex font-semibold text-[12px]'>{label}</span>
+    </button>
   )
 }
 
 MobileShare.propTypes = {
   label: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string,
   weblink: PropTypes.string.isRequired,
-  textColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string,
+  post: PropTypes.object,
+}
+
+MobileShare.defaultProps = {
+  icon: null,
+  textColor: '',
 }
 
 export default MobileShare
