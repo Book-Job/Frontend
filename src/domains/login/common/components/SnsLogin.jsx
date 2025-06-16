b0b28e5cbe468fda89ebc928694fe90f690f7f41
EunSeok-222 <dldmstjr114@naver.com>
2025-06-16 22:18:25 +0900

    fix:소셜 로그인 글씨 반응형 변경

--- src/domains/login/common/components/SnsLogin.jsx
+++ src/domains/login/common/components/SnsLogin.jsx
@@ -1,12 +1,13 @@
 import KakaoLogin from './KakaoLogin'
 import NaverLogin from './NaverLogin'
 const SnsLogin = () => {
-
   return (
     <div className='mt-11'>
       <div className='flex items-center justify-center gap-5'>
         <hr className='w-1/3 border-1 border-dark-gray' />
-        <span className='text-lg sm:text-2xl text-dark-gray font'>소셜 계정으로 간편 로그인</span>
+        <span className='text-lg whitespace-normal sm:text-2xl text-dark-gray break-keep sm:max-w-none sm:whitespace-nowrap'>
+          소셜 계정으로 간편 로그인
+        </span>
         <hr className='w-1/3 border-1 border-dark-gray' />
       </div>
       <div className='flex justify-center gap-16 mt-9'>
