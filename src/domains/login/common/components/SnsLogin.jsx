a37f557ee27f50dd49e57f962461dc6747e384c8
EunSeok-222 <dldmstjr114@naver.com>
2025-06-19 16:45:15 +0900

    wip:중간저장

--- src/domains/login/common/components/SnsLogin.jsx
+++ src/domains/login/common/components/SnsLogin.jsx
@@ -2,15 +2,15 @@ import KakaoLogin from './KakaoLogin'
 import NaverLogin from './NaverLogin'
 const SnsLogin = () => {
   return (
-    <div className='mt-11'>
-      <div className='flex items-center justify-center gap-5'>
+    <div className='mt-5 '>
+      {/* <div className='flex items-center justify-center gap-5'>
         <hr className='w-1/3 border-1 border-dark-gray' />
         <span className='text-lg whitespace-normal sm:text-2xl text-dark-gray break-keep sm:max-w-none sm:whitespace-nowrap'>
-          소셜 계정으로 간편 로그인
+          간편 로그인
         </span>
         <hr className='w-1/3 border-1 border-dark-gray' />
-      </div>
-      <div className='flex justify-center gap-16 mt-9'>
+      </div> */}
+      <div className='flex flex-col gap-5'>
         <NaverLogin />
         <KakaoLogin />
       </div>
