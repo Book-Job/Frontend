0af6618a28854f6a2dc809d3c347dabdcf58e0fb
subinsong01 <haely97@naver.com>
2025-06-23 13:02:23 +0900

    fix: url 수정

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -2,4 +2,4 @@ export const HELP_DESK_URL =
   'https://docs.google.com/forms/d/e/1FAIpQLScMzPL_8D56hPRXe-Y3a8iBu4LF9VCTUUd63EnMGtdMCmS_0A/viewform?usp=header'
 
 export const CUSTOMER_INQUIRY =
-  'https://docs.google.com/forms/d/13ovxKScjj1eOxRQVz-b5-kE6Osiv_FPkB1aA35wzDmw/edit'
+  'https://docs.google.com/forms/d/13ovxKScjj1eOxRQVz-b5-kE6Osiv_FPkB1aA35wzDmw/viewform'
