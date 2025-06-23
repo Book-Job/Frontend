8a8e8fce483913037a8bbca6d23f45a735687801
subinsong01 <haely97@naver.com>
2025-06-23 22:16:47 +0900

    style: 구인 & 구직 workBoard 사이즈 통일

--- src/domains/job/common/components/JobInfiniteScroll.jsx
+++ src/domains/job/common/components/JobInfiniteScroll.jsx
@@ -33,7 +33,7 @@ const JobInfiniteScroll = ({
       {renderList ? (
         renderList(posts)
       ) : (
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl'>
           <JobPostList posts={posts} navigate={navigate} />
         </div>
       )}
