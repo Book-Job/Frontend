1b88c78ca77474c00019da4860ffcdc00a878242
EunSeok-222 <dldmstjr114@naver.com>
2025-07-02 01:04:43 +0900

    style: 소셜 로그인 버튼 간격 수정

--- src/domains/login/page/LoginMain.jsx
+++ src/domains/login/page/LoginMain.jsx
@@ -2,7 +2,7 @@ import LoginForm from './../common/components/LoginForm'
 import SnsLogin from './../common/components/SnsLogin'
 const LoginMain = () => {
   return (
-    <div className='items-center w-full h-full '>
+    <div className='items-center w-full h-full'>
       <LoginForm />
       <SnsLogin />
     </div>
