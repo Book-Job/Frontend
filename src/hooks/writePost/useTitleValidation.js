5a1ffa24a448f721e274f5a913d038b75dbe566d
subinsong01 <haely97@naver.com>
2025-07-11 16:37:56 +0900

    feat: 글 작성 제목 제한 설정

---
+++ src/hooks/writePost/useTitleValidation.js
@@ -0,0 +1,13 @@
+import { useForm } from 'react-hook-form'
+
+export const MAX_TITLE_LENGTH = 30
+
+export default function useTitleValidation() {
+  return {
+    required: '제목을 입력해주세요.',
+    maxLength: {
+      value: MAX_TITLE_LENGTH,
+      message: `제목은 ${MAX_TITLE_LENGTH}자 이내로 작성해주세요.`,
+    },
+  }
+}
