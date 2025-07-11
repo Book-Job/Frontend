c706a40c8f0a6ebd1b6319227aef4002cc349e88
subinsong01 <haely97@naver.com>
2025-07-11 17:06:25 +0900

    fix: 코드래빗 피드백 적용

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,8 +1,11 @@
 import BannerImage from '../../assets/banner/pwa_banner.jpg'
 
-const Banner = () => {
+const Banner = ({ className = '' }) => {
   return (
-    <div className='block cursor-pointer overflow-hidden' aria-label='설문조사 폼으로 이동'>
+    <div
+      className={`block cursor-pointer overflow-hidden ${className}`}
+      aria-label='설문조사 폼으로 이동'
+    >
       <img
         src={BannerImage}
         alt='설문조사 참여 안내 배너'
