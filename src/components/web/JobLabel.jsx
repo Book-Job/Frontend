5677b48285a4c8d88d5a698fdafafb956c91dbb1
subinsong01 <haely97@naver.com>
2025-07-22 02:12:08 +0900

    fix: * 옆에 mr 값 추가

--- src/components/web/JobLabel.jsx
+++ src/components/web/JobLabel.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const JobLabel = ({ label, dot, className }) => {
   return (
     <div className='flex items-center'>
-      {dot && <span className='text-error-red text-sm font-semibold'>*</span>}
+      {dot && <span className='text-error-red text-sm font-semibold mr-1'>*</span>}
       <span className={`font-semibold ${className} mr-[20px] inline-block`}>{label}</span>
     </div>
   )
