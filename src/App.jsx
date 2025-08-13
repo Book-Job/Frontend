97aeee7c35d9ce79b5ae9b26b7ea3c8a2b401c38
EunSeok-222 <dldmstjr114@naver.com>
2025-08-13 23:46:02 +0900

    fix: App.jsx 수정 원본으로 수정함

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,17 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import PwaPopUp from './components/common/PwaPopUp'
-import ReactGA from 'react-ga4'
 
 const queryClient = new QueryClient()
-ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID)
-
 function App() {
   useEffect(() => {
     useAuthStore.getState().initialize()
   }, [])
 
-  const TrackPageViews = () => {
-    const location = useLocation()
-
-    useEffect(() => {
-      ReactGA.send({
-        hitType: 'pageview',
-        page: location.pathname + location.search,
-      })
-    }, [location])
-
-    return null
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <PwaPopUp />
       <BrowserRouter>
-        <TrackPageViews />
         <ToastContainer
           position='bottom-center'
           autoClose={2000}
