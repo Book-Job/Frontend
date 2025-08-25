d06cf64e27f9ac30f68881506fac4ecc89d02d79
subinsong01 <haely97@naver.com>
2025-08-25 22:25:35 +0900

    style: hover 색상 tailwind 커스텀 네임명으로 수정

--- src/domains/error/page/ErrorPage.jsx
+++ src/domains/error/page/ErrorPage.jsx
@@ -6,11 +6,11 @@ const ErrorPage = () => {
 
   return (
     <section className='flex flex-col items-center justify-center text-center px-4'>
-      <h1 className='text-2xl font-bold mb-4'>404 - 페이지를 찾을 수 없습니다 😢</h1>
+      <h1 className='text-2xl font-bold mb-4'>페이지를 찾을 수 없습니다 😢</h1>
       <p className='text-dark-gray mb-8'>주소가 잘못되었거나, 존재하지 않는 페이지입니다.</p>
       <button
         onClick={() => navigate(ROUTER_PATHS.MAIN_PAGE)}
-        className='px-6 py-2 bg-main-pink text-white rounded-lg hover:bg-pink-500 transition-colors'
+        className='px-6 py-2 bg-main-pink text-white rounded-lg hover:bg-hover-pink transition-colors'
         aria-label='홈으로 이동하기'
       >
         홈으로 이동
