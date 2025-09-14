e10c55c62feba37cbfc81725fe5d0fec72681e20
EunSeok-222 <dldmstjr114@naver.com>
2025-09-15 00:06:45 +0900

    style:최신글 버튼 추가

--- src/domains/main/common/components/BoardButton.jsx
+++ src/domains/main/common/components/BoardButton.jsx
@@ -2,14 +2,14 @@ import { useNavigate } from 'react-router-dom'
 import commonTime from '../../../../assets/icons/common/common_time.svg'
 
 const BoardButton = ({ onBoardSelect, selectedBoard, handleRefresh }) => {
-  const boardButton = [{ name: '자유' }, { name: '구인구직' }]
+  const boardButton = [{ name: '구인구직' }, { name: '자유' }, { name: '최신글' }]
   const navigate = useNavigate()
   const today = new Date()
   const formattedDate = `${today.getMonth() + 1}월 ${today.getDate()}일`
 
   const handleClick = (path, name) => {
     navigate(path)
-    onBoardSelect(name === '자유' ? '자유게시판' : '구인구직')
+    onBoardSelect(name === '자유' ? '자유게시판' : name === '구인구직' ? '구인구직' : '최신글')
   }
   return (
     <div className='flex justify-center w-full max-w-[940px] mx-auto'>
@@ -18,7 +18,8 @@ const BoardButton = ({ onBoardSelect, selectedBoard, handleRefresh }) => {
           {boardButton.map(({ name, path }, index) => {
             const isSelected =
               (name === '자유' && selectedBoard === '자유게시판') ||
-              (name === '구인구직' && selectedBoard === '구인구직')
+              (name === '구인구직' && selectedBoard === '구인구직') ||
+              (name === '최신글' && selectedBoard === '최신글')
             return (
               <button
                 key={index}
