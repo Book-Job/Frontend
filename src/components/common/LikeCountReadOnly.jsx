96833e5f88836a175f2aa3384ccc6ea3d9c20ed2
subinsong01 <haely97@naver.com>
2025-09-24 00:02:34 +0900

    feat: 읽기 전용 좋아요 생성

---
+++ src/components/common/LikeCountReadOnly.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import GoodIcon from '../../assets/icons/common/common_good.svg'
+
+const LikeCountReadOnly = ({ count, className }) => {
+  return (
+    <div className={`flex items-center mb-1 ${className}`}>
+      <img src={GoodIcon} alt='좋아요' className='w-4 h-4 mt-1 md:w-4 md:h-4' />
+      <span className='flex mt-1 ml-1'>{count}</span>
+    </div>
+  )
+}
+
+LikeCountReadOnly.propTypes = {
+  count: PropTypes.number.isRequired,
+  className: PropTypes.string,
+}
+
+export default LikeCountReadOnly
